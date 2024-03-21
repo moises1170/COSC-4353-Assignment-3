93e769edf53c071175156046520b9bd0a06428ce
moises1170 <moises.palacios1170@gmail.com>
2024-03-20 19:43:00 -0500

    updated privatekey and added .env to .gitignore

--- fuelvisionary/fuelvisionary/settings.py
+++ fuelvisionary/fuelvisionary/settings.py
@@ -18,14 +18,12 @@ SECRET_KEY = config("SECRET_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 

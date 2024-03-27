c6eecd67b5cf6d368427807dcf5f4b7d24942563
Abel Barcenas <abarcenasjr@outlook.com>
2024-03-27 16:24:37 -0500

    hoping to fix errors

--- fuelvisionary/quotes/models.py
+++ fuelvisionary/quotes/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 # Create your models here.
 
-class FuelQuoteRequest(models.Model):
-    requestedGallons = models.CharField(max_length = 10)
-    deliveryDate = models.DateField(null = True)
-
+class FuelQuote(models.Model):
+    requested_gallons = models.PositiveIntegerField()
+    delivery_date = models.DateField()
+    delivery_address = models.CharField(max_length=255)
+    price_per_gallon = models.DecimalField(max_digits=10, decimal_places=2)
+    total_amount_due = models.DecimalField(max_digits=20, decimal_places=2)
+    client = models.ForeignKey('profile.Client', on_delete=models.CASCADE)

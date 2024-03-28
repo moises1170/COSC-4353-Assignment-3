5b60fbdab3ee82c3e957b4d2471c817eceaae7a8
Abel Barcenas <abarcenasjr@outlook.com>
2024-03-28 00:17:16 -0500

    midnight attempt

--- fuelvisionary/quotes/models.py
+++ fuelvisionary/quotes/models.py
@@ -1,10 +1,13 @@
 from django.db import models
-# Create your models here.
+from django.core.validators import MinLengthValidator
+from profile.models import client
 
-class FuelQuote(models.Model):
-    requested_gallons = models.PositiveIntegerField()
-    delivery_date = models.DateField()
-    delivery_address = models.CharField(max_length=255)
-    price_per_gallon = models.DecimalField(max_digits=10, decimal_places=2)
-    total_amount_due = models.DecimalField(max_digits=20, decimal_places=2)
-    client = models.ForeignKey('profile.Client', on_delete=models.CASCADE)
+# Create your models here.
+# class for the fuel quotes
+class fuel_quote(models.Model):
+    gallonsRequested = models.PositiveIntegerField()
+    deliveryAddress = models.CharField(max_length=100)
+    deliveryDate = models.DateField()
+    priceQuote = models.DecimalField(decimal_places=2, default=10, max_digits=10)
+    amountDue = models.DecimalField(decimal_places=2, max_digits=10)
+    client = models.ForeignKey(client, on_delete=models.CASCADE)
--- fuelvisionary/quotes/views.py
+++ fuelvisionary/quotes/views.py
@@ -1,6 +1,26 @@
-# Create your views here.
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import fuel_quote, client
+from .forms import FuelQuoteForm  # Make sure to create this form
+
+
+def fuelQuote(request):
+    user_client = client.objects.get(client=request.user)
+    if request.method == 'POST':
+        form = FuelQuoteForm(request.POST)
+        if form.is_valid():
+            new_quote = form.save(commit=False)
+            new_quote.client = user_client
+            # Assuming price and total due are calculated within the form's save method or elsewhere
+            new_quote.save()
+            return redirect('quoteHistory')  # Redirect to the history view or another appropriate page
+    else:
+        form = FuelQuoteForm()
+
+    return render(request, 'fuel_quote.html', {'form': form})
 
 def quoteHistory(request):
-    return render(request, 'fuel_quote.html')
+    user_client = client.objects.get(client=request.user)
+    fuel_quotes = fuel_quote.objects.filter(client=user_client)
+    return render(request, 'fuelQuoteHistory.html', {'fuel_quotes': fuel_quotes})

136f679f220c6df1837b9a7d2200c9b2a4dc88cb
Hannah Hebert <112514427+HaHebert@users.noreply.github.com>
2024-03-27 22:18:20 -0500

    implemented communication between fuel quote form and fuel quote history page

--- fuelvisionary/quotes/models.py
+++ fuelvisionary/quotes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from django.contrib.auth.models import User
 
 # Create your models here.
+# class for the fuel quotes
+class fuel_quote(models.Model):
+    gallonsRequested = models.PositiveIntegerField()
+    deliveryAddress = models.CharField(max_length=100)
+    deliveryDate = models.DateField()
+    priceQuote = models.DecimalField(decimal_places=2, default=10, max_digits=10)
+    amountDue = models.DecimalField(decimal_places=2, max_digits=10)
+    client = models.OneToOneField(User, on_delete=models.CASCADE)
--- fuelvisionary/quotes/views.py
+++ fuelvisionary/quotes/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from profile.models import client
+from .models import fuel_quote
 
 # Create your views here.
 def fuelQuote(request):
+    if request.method == 'POST':
+        gallonsRequested = request.POST.get("gallonsRequested")
+        deliveryDate = request.POST.get("deliveryDate")
+        priceQuote = 10
+        totalDue = request.POST.get("totalDue")
+        user_profile = client.objects.get(client=request.user)
+        deliveryAddress = f"{user_profile.address1}, {user_profile.address2}, {user_profile.city}, {user_profile.state}, {user_profile.zipcode}"
+        new_quote = fuel_quote.objects.create(gallonsRequested=gallonsRequested, deliveryAddress=deliveryAddress, deliveryDate=deliveryDate, priceQuote=priceQuote, totalDue=totalDue)
+        
     return render(request, 'fuel_quote.html')
-
+  
 def quoteHistory(request):
-    return render(request, 'fuelQuoteHistory.html')
+    fuel_quotes = fuel_quote.objects.filter(client=request.user)
+    return render(request, 'fuelQuoteHistory.html', {'fuel_quotes': fuel_quotes})

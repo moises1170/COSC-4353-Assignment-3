bfb197124607871e2907bd32264d06b8ad9ad222
moises1170 <moises.palacios1170@gmail.com>
2024-03-29 20:16:43 -0500

    changed app name to price and model to Pricing

--- fuelvisionary/fuelvisionary/settings.py
+++ fuelvisionary/fuelvisionary/settings.py
@@ -27,7 +27,6 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,7 +37,7 @@ INSTALLED_APPS = [
     'profile',
     'quotes',
     'users',
-    'pricing'
+    'price'
 ]
 
 MIDDLEWARE = [

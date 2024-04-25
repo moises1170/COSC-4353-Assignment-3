6365a34202ac92d311a29397f8ced55127109980
Palacioem <palacio720@gmail.com>
2024-04-24 23:17:07 -0500

    register.html changes and logIn.js changes

--- fuelvisionary/users/static/js/logIn.js
+++ fuelvisionary/users/static/js/logIn.js
@@ -2,6 +2,8 @@
 function validationCheck(event) { //function validates that form has been filled before submiting 
     var inputs = document.querySelectorAll(".input");
     var errorMessage= document.querySelectorAll(".error");
+    var email = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
     var isvalid = true;
     // Example validation: Checking if both username and password are not empty
     for(let i=0; i<inputs.length; i++){
@@ -11,6 +13,16 @@ function validationCheck(event) { //function validates that form has been filled
             event.preventDefault();
         }
     }
+    // Example validation: Checking if email is valid  
+    if (!validateEmail(email) && email.trim() !== ""){
+        document.getElementById("errorMessage").textContent = "Please enter a valid email";
+        isvalid = false;
+    }
+    if(password.length < 8 && password.trim() !== ""){
+        document.getElementById("errorMessage").textContent = "Password must be at least 8 characters";
+        isvalid = false;
+    }
+    
     if (!isvalid) {
         event.preventDefault(); // Prevent form submission
     } else {
@@ -19,12 +31,23 @@ function validationCheck(event) { //function validates that form has been filled
     }
  
     
-}
+    }
+    function validateEmail(email) {
+        var regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return regex.test(email);
+    }
 
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("formLogin").addEventListener("submit", validationCheck)
     //clear error message if usesr tries again 
     document.getElementById("username").addEventListener("input", function() {
+        var errors = document.querySelectorAll(".error")
+        for(var error of errors){
+            error.textContent="";
+        }
+    
+    });
+    document.getElementById("password").addEventListener("input", function() {
         var errors = document.querySelectorAll(".error")
         for(var error of errors){
             error.textContent="";
